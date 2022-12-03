f5307bda410e649ea56a4be6b8b0194e8af512bd
Yury Pidenko <kenjizz@mail.ru>
2022-12-03 14:14:31 +0300

    DZ

---
+++ DZ_CSharp02/dz01/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//  456 -> 5
+//  782 -> 8
+//  918 -> 1
+
+
+Console.WriteLine("Ведите трех значное число");
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
