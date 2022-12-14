f026e4648e07753d62d615c37d1b242227aab1a7
Yury Pidenko <kenjizz@mail.ru>
2022-12-14 20:09:29 +0300

    выаолнил dz04

---
+++ DZ_CSharp04/dz01/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+Не использовать метод Math.Pow
+3, 5 -> 243 (3⁵)
+2, 4 -> 16   */
+
+Console.WriteLine("введите число ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите степень числа ");
+double b = Convert.ToDouble(Console.ReadLine());
+double result = a;
+if(a == 0 && b == 0)
+{
+    Console.WriteLine(1);
+}
+else 
+{
+    exponentiationFunction(a, b);
+}
+
+
+
+void exponentiationFunction (double a, double b)
+{
+for (int i = 1; i < b; i++)
+{
+    result = result * a;
+}
+Console.WriteLine($"A в степени B равно: {result}");
+System.Console.WriteLine($"отрецательная степеь {1/result}");
+}
---
+++ DZ_CSharp04/dz03/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 3: (branch task_3)
+Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]
+*/
+
+
+Console.WriteLine("Введите длинну массива:");
+int array = int.Parse(Console.ReadLine());
+
+int[] randomArray = new int[array];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+
