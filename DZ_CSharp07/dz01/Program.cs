dbfed8326bf42608a244e25166ae5443bcd35a49
Yury Pidenko <kenjizz@mail.ru>
2022-12-24 15:53:47 +0300

    dz07

---
+++ DZ_CSharp07/dz01/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+
+
+
+int lines = ConsoleInput("Введите количество строк  ");
+int columns = ConsoleInput("Введите количество столбцов ");
+double[,] numbers = new double[lines, columns];
+ArrayRandomNumbers(numbers);
+ConsoleArray(numbers);
+
+void ArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void ConsoleArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+
+
+
+int ConsoleInput(string message)  
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ DZ_CSharp07/dz03/Program.cs
@@ -0,0 +1,68 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+
+
+int lines = ConsoleInput("Введите количество строк ");
+int columns = ConsoleInput("Введите количество столбцов ");
+
+int[,] numbers = new int[lines, columns];
+ArrayRandomNumbers(numbers);
+
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double ArefmeticalMean = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        ArefmeticalMean = (ArefmeticalMean + numbers[i, j]);
+    }
+    ArefmeticalMean = ArefmeticalMean / lines;
+    Console.Write(ArefmeticalMean + "; ");
+}
+Console.WriteLine();
+ConsoleArray(numbers);
+
+
+
+
+
+void ArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+
+
+void ConsoleArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+
+
+int ConsoleInput(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
